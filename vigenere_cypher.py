d4d73b0f345c8b0517ce358ea407347398585d0f
Psh <pyaesonehein81998@gmail.com>
2025-04-16 18:11:46 +0630

    [ADD] encrypt and decrypt function

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -9,14 +9,22 @@ def vigenere(message, key, direction): #+1 for encrypt -1 for descrypt
         if char == ' ':
             encrypted_text += char
         else:        
-            # Find the right key character to encode
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted letter
+            # Define the offset and the encrypted/decrypted letter 
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset*direction) % len(alphabet)
             encrypted_text += alphabet[new_index]
     
     return encrypted_text
+
+
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
