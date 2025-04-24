4db3bbdbde8715ea8150a599db27ff3a40b1f9a9
Psh <pyaesonehein81998@gmail.com>
2025-04-24 22:07:49 +0630

    [ADD] shortest_path.py

---
+++ shortest_path.py
@@ -0,0 +1,16 @@
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    
+
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+}    
+shortest_path(my_graph, 'A')
